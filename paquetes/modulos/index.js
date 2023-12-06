461bf5da6f63aedd62bffd040e9f8a454a9c9713
LittleBigPants <juanmanuel_garcia98@hotmail.com>
2023-12-06 12:40:22 -0300

    modulos

---
+++ paquetes/modulos/index.js
@@ -0,0 +1,9 @@
+// traer nuestro modulo
+const saludar = require("./modulo");
+const modulo = require("./modulo");
+
+// ejecutar una funcion del modulo
+console.log(modulo.prop1);
+modulo.saludar();
+// saludar();
+// modulo();
